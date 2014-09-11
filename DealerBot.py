49ab7702abb7fbc97d31c93305155c6aabb4793b
soniloi <niloinso@tcd.ie>
2014-09-11 13:47:36 +0100

    Got whoising/joining/quitting working

--- DealerBot.py
+++ DealerBot.py
@@ -4,7 +4,7 @@ from Player import Player
 
 # twisted imports
 from twisted.words.protocols import irc
-from twisted.internet import reactor, protocol
+from twisted.internet import reactor, protocol, defer
 from twisted.python import log
 
 # system imports
@@ -34,6 +34,7 @@ class DealerBot(irc.IRCClient):
 
     players = {}
     joinindex = 0
+    whoisinfo = {}
 
     def isPlayer(self, nick):
         for k, v in self.players.iteritems():
@@ -42,18 +43,35 @@ class DealerBot(irc.IRCClient):
         return False
  
     def doJoin(self, msg, channel, user):
-        if self.isPlayer(user):
-            self.msg(user, "You are already playing!")
-        elif len(self.players) >= Config.PLAYERS_MAX:
-            msg = user + ": No more than " + str(Config.PLAYERS_MAX) + " players permitted."
+        if not self.isPlayer(user): # Ignore joiner who has already joined
+            if len(self.players) >= Config.PLAYERS_MAX:
+                msg = user + ": No more than " + str(Config.PLAYERS_MAX) + " players permitted."
+                self.msg(channel, msg)
+            else:
+                d = self.whois(user)
+                d.addCallback(self.addPlayer, user, channel)
+
+    def doQuit(self, msg, channel, user):
+        if self.isPlayer(user): # Ignore quitter who is not already playing
+            for k, player in self.players.iteritems():
+                if player.nick == user:
+                    del self.players[k]
+                    break
+            msg = user + " has left the game. "
+            if not self.players:
+                msg += "No players remaining."
+            else:
+                msg += str(len(self.players)) + " player(s) remaining."
             self.msg(channel, msg)
-        else:
-            msg = user + " is attempting to join the game."
-            self.msg(channel, msg)
-            whois = self.whois(user)
-            # hostmask = result of /whois user; hostmask = self.whois(%s)?
-            # players[joinindex] = Player(hostmask, <%s>)
-            # joinindex = joinindex + 1
+
+    def addPlayer(self, mask, nick, channel):
+        self.players[self.joinindex] = Player(mask, nick)
+        self.joinindex += 1
+        print nick + " joining."
+        for k, player in self.players.iteritems():
+          print player.nick + " " + player.hostmask
+        msg = nick + " has joined the game and raised the number of players to " + str(len(self.players)) + "."
+        self.msg(channel, msg)
 
     def doStart(self, msg, channel, user):
         # Someone attempts to start  
@@ -62,13 +80,16 @@ class DealerBot(irc.IRCClient):
                 msg = user + ": Minimum of " + str(Config.PLAYERS_MIN) + " players required to start a game."
                 self.msg(channel, msg)
             else:
-                self.start()
+                self.start(channel)
 
     def doStats(self, msg, channel, user):
         pass    
 
     commands = { "j" : doJoin,
                  "join" : doJoin,
+                 "leave" : doQuit,
+                 "q" : doQuit,
+                 "quit" : doQuit,
                  "start" : doStart,
                  "stats" : doStats }
 
@@ -84,10 +105,33 @@ class DealerBot(irc.IRCClient):
                         time.asctime(time.localtime(time.time())))
         self.logger.close()
 
+    # Whois wrapper
+    def whois(self, nick):
+        d = defer.Deferred()
+        if nick in self.whoisinfo:
+            ds = self.whoisinfo[nick][0]
+            ds.append(d)
+            return d
+  
+        info = {}
+        self.whoisinfo[nick] = [[d], info]
+        irc.IRCClient.whois(self, nick, None)
+        return d
+
     # callbacks for events
 
     def irc_RPL_WHOISUSER(self, prefix, params):
-        print params
+        nick = params[1]
+        mask = params[2] + '@' + params[3]
+        self.whoisinfo[nick][1] = mask
+  
+    def irc_RPL_ENDOFWHOIS(self, prefix, params):
+        nick = params[1].lower()
+        if nick in self.whoisinfo:
+            ds = self.whoisinfo[nick][0]
+            info = self.whoisinfo[nick][1]
+            del self.whoisinfo[nick]
+            [d.callback(info) for d in ds]
 
     def signedOn(self):
         """Called when bot has succesfully signed on to server."""
@@ -143,7 +187,12 @@ class DealerBot(irc.IRCClient):
         """
         return nickname + '^'
 
-    def start(self):
+    def start(self, channel):
+        welcomemsg = ""
+        for k, player in self.players.iteritems():
+            welcomemsg += player.nick + ", "
+        welcomemsg = welcomemsg[:-2] + ": Welcome to Cards Against Apples."
+        self.msg(channel, welcomemsg)
         game = Game(self.players)
         game.play()
 
